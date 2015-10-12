3c8ba2e1fbbb39a4340bfa410882d3f77761d3b2
artemkaint <artemkaint@nodasoft.com>
2015-10-12 12:06:38 +0300

    Implement Module load

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var reactTemplates = require('react-templates/src/reactTemplates');
-var url = require('url');
+var path = require('path');
 var fs = require('fs');
 var ext = '.rt';
 
@@ -12,6 +12,32 @@ var loadRtFile = function(module, filename) {
 
 if (require.extensions !== undefined) {
   require.extensions[ext] = loadRtFile;
+
+  var Module = require('module');
+
+  var findExtension = function(filename) {
+    var extensions = path.basename(filename).split('.');
+    if (extensions[0] === '') {
+      extensions.shift()
+    }
+
+    while (extensions.shift()) {
+      var curExtension = '.' + extensions.join('.');
+      if (Module._extensions[curExtension]) {
+        return curExtension;
+      }
+    }
+    return '.js';
+  };
+
+  Module.load = function(filename) {
+    this.filename = filename;
+    this.paths = Module._nodeModulePaths(path.dirname(filename));
+    var extension = findExtension(filename);
+    Module._extensions[extension](this, filename);
+    this.loaded = true
+    return this.loaded;
+  }
 }
 
 module.exports = loadRtFile;
