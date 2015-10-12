62d3912850f0110d4cfc08447042d925d7f10808
artemkaint <artemkaint@nodasoft.com>
2015-10-12 12:30:39 +0300

    Implement rt compile options

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 var reactTemplates = require('react-templates/src/reactTemplates');
-var path = require('path');
 var fs = require('fs');
+var _ = require('lodash');
 var ext = '.rt';
 
-var loadRtFile = function(module, filename) {
-  var options = {};
-  var source = fs.readFileSync(filename, 'utf8');
-  var answer = reactTemplates.convertTemplateToReact(source, options);
-  module._compile(answer, filename);
-};
+module.exports = function(options) {
+  var defaultOptions = {
+    modules: 'commonjs'
+  };
+  var options = _.extend({}, defaultOptions, options);
 
-if (require.extensions !== undefined) {
-  require.extensions[ext] = loadRtFile;
-
-  var Module = require('module');
-
-  var findExtension = function(filename) {
-    var extensions = path.basename(filename).split('.');
-    if (extensions[0] === '') {
-      extensions.shift()
-    }
-
-    while (extensions.shift()) {
-      var curExtension = '.' + extensions.join('.');
-      if (Module._extensions[curExtension]) {
-        return curExtension;
-      }
-    }
-    return '.js';
+  var loadRtFile = function(module, filename) {
+    var source = fs.readFileSync(filename, 'utf8');
+    var answer = reactTemplates.convertTemplateToReact(source, options);
+    module._compile(answer, filename);
   };
 
-  Module.load = function(filename) {
-    this.filename = filename;
-    this.paths = Module._nodeModulePaths(path.dirname(filename));
-    var extension = findExtension(filename);
-    Module._extensions[extension](this, filename);
-    this.loaded = true
-    return this.loaded;
+  if (require.extensions !== undefined) {
+    require.extensions[ext] = loadRtFile;
   }
-}
-
-module.exports = loadRtFile;
+};
