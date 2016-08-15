4d6b8cfe0117730b41e584d46526a88511531bf4
artemkaint <artemkaint@nodasoft.com>
2016-08-15 09:43:42 +0000

    ES6

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-var reactTemplates = require('react-templates/src/reactTemplates');
-var fs = require('fs');
-var _ = require('lodash');
-var ext = '.rt';
+import reactTemplates from 'react-templates/src/reactTemplates';
+import fs from 'fs';
+const ext = '.rt';
 
-module.exports = function(options) {
-  var defaultOptions = {
+export default function hook(options) {
+  const convertOptions = Object.assign({}, {
     modules: 'commonjs',
     reactImportPath: 'react'
-  };
-  var convertOptions = _.extend({}, defaultOptions, options);
+  }, options);
 
-  var loadRtFile = function(module, filename) {
+  function loadRtFile (module, filename) {
     var source = fs.readFileSync(filename, 'utf8');
     var answer = reactTemplates.convertTemplateToReact(source, convertOptions);
     module._compile(answer, filename);
-  };
+  }
 
   if (require.extensions !== undefined) {
     require.extensions[ext] = loadRtFile;
